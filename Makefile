a2b21f8da2baa1f04e897c761cdf9fda33f41e90
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-12-18 10:16:00 -0600

    working

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-IMAGE=mcandre/docker-solr:3.4.0
+IMAGE=mcandre/docker-solr:3.3.0
 
 export LOCALHOST=$$(docker-machine ip default)
 
--- start-solr.sh
+++ start-solr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd /apache-solr-3.4.0/example
+cd /apache-solr-3.3.0/example
 java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -jar start.jar
